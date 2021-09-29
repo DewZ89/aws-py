df393f1bb95e023149b742203162bef458c07fe4
Martial Anouman <80514220+DewZ89@users.noreply.github.com>
2021-09-29 16:00:41 +0000

    update .sh file

--- boostrap.sh
+++ boostrap.sh
@@ -8,13 +8,13 @@ sudo add-apt-repository -y ppa:deadsnakes/ppa
 
 sudo apt-get install -y python3.8 pip
 
-mkdir -p /var/app
+sudo mkdir -p /var/app
 
 cd /var/app
 
-git clone https://github.com/ .
+git clone https://github.com/DewZ89/aws-py.git .
 
-pip install -r requirements.txt --no-cache-dir
+pip3 install -r requirements.txt --no-cache-dir
 
 uvicorn main:app --host=0.0.0.0 --port=80
 
